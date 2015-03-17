c90f91781751988b8e2edb6223a0e3548d86abe6
Matthew Basanta <matthew.basanta@gmail.com>
2015-03-17 16:38:47 -0400

    Add a model for QR5 entries

--- qr5server/__init__.py
+++ qr5server/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-import qr5server.apiroutes
-import qr5server.errorhandlers
-
+from qr5server import apiroutes, errorhandlers, models
---
+++ qr5server/models.py
@@ -0,0 +1,20 @@
+from qr5server import db
+
+class QR5Record(db.Model):
+    id = db.Column(db.String(40), primary_key=True)
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+    dfirm_feat_id = db.Column(db.Integer)
+    dfirm_layer = db.Column(db.String(50))
+    firm_panel = db.Column(db.String(255))
+    error_code = db.Column(db.String(6))
+    error_desc= db.Column(db.String(255))
+    qc_reviewer = db.Column(db.String(30))
+    qc_status = db.Column(db.String(3))
+    changes_made = db.Column(db.String(30))
+    changes_verified = db.Column(db.String(30))
+    comments = db.Column(db.String(255))
+    response = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<QR5Record %r>' % self.id
