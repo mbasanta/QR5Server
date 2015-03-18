71749cb858d2e8d91573956470cb6f19969a28fb
Matthew Basanta <matthew.basanta@gmail.com>
2015-03-18 15:19:32 -0400

    Update config to set pagination defaults

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+'''Application Configuration'''
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+import os
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+# SQL Alchemy
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(BASEDIR, 'db_repository')
+
+# Pagination
+RECORDS_PER_PAGE = 10
