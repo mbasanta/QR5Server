7521d1d601a6b4496b77c344f76d2107f979ca74
Matthew Basanta <matthew.basanta@gmail.com>
2015-04-22 11:35:51 -0400

    Add authentication methods

--- qr5server/__init__.py
+++ qr5server/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.ext.httpauth import HTTPBasicAuth
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import qr5server.routes.geojson
@@ -7,8 +8,11 @@ app = Flask(__name__)
 app.config.from_object('config.DebugConfiguration')
 app.register_blueprint(qr5server.routes.geojson.geojson_api)
 
+auth = HTTPBasicAuth()
+
 cors = CORS(app, allow_headers='Origin, X-Requested-With, Content-Type, Accept')
 
 db = SQLAlchemy(app)
 
-from qr5server import apiroutes, errorhandlers, models
+from qr5server import apiroutes, errorhandlers
+from qr5server.helpers.auth import verify_password, auth_error
